0597402f82f276e45c64ee751bd19ba307dd8875
erinbedri <erinbedri@gmail.com>
2023-01-27 12:39:37 +0100

    welcome message on homepage for auth users

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useContext } from "react";
+
+import "./home.css";
+import { AuthContext } from "../../contexts/AuthContext";
 
 export default function Home() {
-  return (
-    <section className="section-bigger-padding centered">
-      <h1>Home Page</h1>
-    </section>
-  );
+    const { user } = useContext(AuthContext);
+    console.log(user);
+
+    return (
+        <section id="home" className="section-bigger-padding centered">
+            <h1>Home Page</h1>
+            {user.accessToken ? (
+                <p style={{ marginTop: "1rem" }}>Welcome back, {user.email}</p>
+            ) : (
+                ""
+            )}
+        </section>
+    );
 }
