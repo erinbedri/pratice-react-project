adb8731af53ab8eb6175beec8526361b08eba972
erinbedri <erinbedri@gmail.com>
2023-01-30 11:28:58 +0100

    populated practice server's default dataset

--- src/services/requester.js
+++ src/services/requester.js
@@ -1,26 +1,36 @@
 const request = async (method, url, data) => {
-  try {
-    let buildRequest;
+    try {
+        const user = localStorage.getItem("auth");
+        const auth = JSON.parse(user || "{}");
 
-    if (method === "GET") {
-      buildRequest = fetch(url);
-    } else {
-      buildRequest = fetch(url, {
-        method,
-        headers: {
-          "content-type": "application/json",
-        },
-        body: JSON.stringify(data),
-      });
+        let headers = {};
+
+        if (auth.accessToken) {
+            headers["X-Authorization"] = auth.accessToken;
+        }
+
+        let buildRequest;
+
+        if (method === "GET") {
+            buildRequest = fetch(url, { headers });
+        } else {
+            buildRequest = fetch(url, {
+                method,
+                headers: {
+                    ...headers,
+                    "content-type": "application/json",
+                },
+                body: JSON.stringify(data),
+            });
+        }
+
+        const response = await buildRequest;
+        const result = await response.json();
+
+        return result;
+    } catch (error) {
+        console.log(`this is error: ${error}`);
     }
-
-    const response = await buildRequest;
-    const result = await response.json();
-
-    return result;
-  } catch (error) {
-    console.log(`this is error: ${error}`);
-  }
 };
 
 export const get = request.bind({}, "GET");
